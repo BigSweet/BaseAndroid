a4a81e9ef24b13cbbb80c1f955186f03b85d6545
sunwentao <sunwentao@primedu.cn>
2017-09-11 17:51:37 +0800

    sunwentao

--- baselib/src/main/java/cn/primedu/m/baselib/base/SWBaseActivity.java
+++ baselib/src/main/java/cn/primedu/m/baselib/base/SWBaseActivity.java
@@ -177,6 +177,7 @@ public abstract class SWBaseActivity extends FragmentActivity {
     }*/
     int requestcode;
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        super.onActivityResult(requestCode, resultCode, data);
