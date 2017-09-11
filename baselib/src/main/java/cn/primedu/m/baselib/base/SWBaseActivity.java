c65ea67d9b660c2fe52ffe1d8a5dab9b4a9c6445
sunwentao <sunwentao@primedu.cn>
2017-09-11 17:06:11 +0800

    sunwentao

--- baselib/src/main/java/cn/primedu/m/baselib/base/SWBaseActivity.java
+++ baselib/src/main/java/cn/primedu/m/baselib/base/SWBaseActivity.java
@@ -179,7 +179,7 @@ public abstract class SWBaseActivity extends FragmentActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+//        super.onActivityResult(requestCode, resultCode, data);
         if (mFragmentManager != null) {
             List<Fragment> fragments = mFragmentManager.getFragments();
             if (fragments != null && fragments.size() > 0) {
