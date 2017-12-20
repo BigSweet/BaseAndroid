681f55422a9bc3de2b19f324d0b038ed6d232a88
sunwentao <sunwentao@primedu.cn>
2017-12-20 15:02:36 +0800

    sunwnt

--- app/src/buy/java/buy/BuyTestActivity.java
+++ app/src/buy/java/buy/BuyTestActivity.java
@@ -1,10 +1,18 @@
 package buy;
 
+import android.support.v4.app.Fragment;
+
+import cn.primedu.m.baselib.base.SWBaseActivity;
+
 /**
  * 介绍：这里写介绍
  * 作者：sweet
  * 邮箱：sunwentao@priemdu.cn
  * 时间: 2017/12/13
  */
-public class BuyTestActivity {
+public class BuyTestActivity extends SWBaseActivity {
+    @Override
+    protected Fragment getFragment() {
+        return null;
+    }
 }
